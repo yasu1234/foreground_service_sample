6f59194c76a2e7f8d380dbeb0a1e0ee28c5ac2aa
神代泰宏 <yasuhiro79367s@hotmail.co.jp>
2020-11-13 10:40:04 +0900

    first commit

---
+++ app/src/main/java/com/example/foreground_service_sample/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.foreground_service_sample
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
